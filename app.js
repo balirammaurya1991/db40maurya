d768d795ae6f97e70da2f2ca70fbabd1e4640447
balirammaurya1991 <89415721+balirammaurya1991@users.noreply.github.com>
2021-11-17 23:40:32 -0600

    updated code

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } ));
 var Costume = require("./models/costume"); 
 // database connection
 const connectionString = process.env.MONGO_CON
@@ -35,6 +50,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -45,7 +69,16 @@ app.use('/selector',selector);
 app.use('/resource',resource);
 app.use('/costume',costume)
 
-// catch 404 and forward to error handler
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account = require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+
+ // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
