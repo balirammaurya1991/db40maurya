b57e99537a95e2ce1319ea36b26d3f729d9db580
balirammaurya1991 <89415721+balirammaurya1991@users.noreply.github.com>
2021-11-27 20:25:55 -0600

    updated code

--- models/costume.js
+++ models/costume.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const costumeSchema = mongoose.Schema({ 
  costume_type: String, 
  size: String, 
- cost: Number 
+ cost:{ type: Number, min: 5, max: 32 }
 },{
     writeConcern: {
       w: 'majority',
--- routes/costumes.js
+++ routes/costumes.js
@@ -4,6 +4,14 @@ const detail_controller =  require('../controllers/constumedetail');
 const createcostume_controller = require('../controllers/createcostume');
 const costumeupdate_controller = require('../controllers/costumeupdate');
 var router = express.Router(); 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+ 
  
 /* GET costumes */ 
 router.get('/', costume_controllers.costume_view_all_Page ); 
@@ -15,7 +23,7 @@ router.get('/detail', detail_controller.costume_view_one_Page);
 /* GET create costume page */ 
 router.get('/create', createcostume_controller.costume_create_Page);
 /* GET create update page */ 
-router.get('/update', costumeupdate_controller.costume_update_Page); 
+router.get('/update', secured, costumeupdate_controller.costume_update_Page); 
 /* GET create costume page */ 
 router.get('/delete', costume_controllers.costume_delete_Page); 
  
